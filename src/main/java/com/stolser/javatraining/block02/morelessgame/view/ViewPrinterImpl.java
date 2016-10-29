0605ef9fc00065ab28c80ef00a121d34df9aa7b1
Stoliarov Oleg <stoljarov.oleg@gmail.com>
2016-10-29 21:59:13 +0300

    Move local from Application to ViewPrinter

--- src/main/java/com/stolser/javatraining/block02/morelessgame/view/ViewPrinterImpl.java
+++ src/main/java/com/stolser/javatraining/block02/morelessgame/view/ViewPrinterImpl.java
@@ -1,15 +1,26 @@
 package com.stolser.javatraining.block02.morelessgame.view;
 
 import java.io.*;
+import java.util.Locale;
 
 class ViewPrinterImpl implements ViewPrinter {
+    private Locale locale;
     private PrintWriter output;
 
     public ViewPrinterImpl(OutputStream output) {
         this.output = new PrintWriter(new BufferedWriter(new OutputStreamWriter(output)));
+        this.locale = DEFAULT_LOCALE;
     }
 
     public void printMessage(String message) {
         output.println(message);
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public void setLocale(Locale locale) {
+        this.locale = locale;
     }
 }
