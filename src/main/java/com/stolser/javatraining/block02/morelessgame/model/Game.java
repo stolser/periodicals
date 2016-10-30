6267b6bdab178bb3ab9edfcf25679501f7f88f81
Stoliarov Oleg <stoljarov.oleg@gmail.com>
2016-10-30 09:51:07 +0200

    Start implementing Game

--- src/main/java/com/stolser/javatraining/block02/morelessgame/model/Game.java
+++ src/main/java/com/stolser/javatraining/block02/morelessgame/model/Game.java
@@ -1,9 +1,16 @@
 package com.stolser.javatraining.block02.morelessgame.model;
 
 public class Game {
-    private static final int RANDOM_MIN_DEFAULT = 0;
-    private static final int RANDOM_MAX_DEFAULT = 100;
+    private static int randomMinDefault = 0;
+    private static int randomMaxDefault = 100;
 
+    public void start() {
+        System.out.println("Start a Game...End the Game.");
+    }
 
+    public static void setRandomMaxDefault(int newValue) {
+        if(newValue < 10 || newValue > 1000) throw new IllegalArgumentException();
+        randomMaxDefault = newValue;
+    }
 
 }
