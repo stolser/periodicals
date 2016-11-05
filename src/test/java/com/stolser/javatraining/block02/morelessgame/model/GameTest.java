7df5f14ba7b18e0fc7d9b00f3e273519950d4d83
Stoliarov Oleg <stoljarov.oleg@gmail.com>
2016-11-05 10:50:34 +0200

    Add test for class Game

--- src/test/java/com/stolser/javatraining/block02/morelessgame/model/GameTest.java
+++ src/test/java/com/stolser/javatraining/block02/morelessgame/model/GameTest.java
@@ -1,5 +1,6 @@
 package com.stolser.javatraining.block02.morelessgame.model;
 
+import com.google.common.collect.Range;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,5 +12,33 @@ public class GameTest {
         Game.setUpperBoundDefault(newValue);
 
         Assert.assertEquals(newValue, Game.getUpperBoundDefault());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setLowerBoundShouldThrowExceptionIfArgumentLessThanMin() {
+        Range<Integer> lowerBoundRange = Game.getLowerBoundLimits();
+
+        Game.setLowerBoundDefault(lowerBoundRange.lowerEndpoint() - 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setLowerBoundShouldThrowExceptionIfArgumentMoreThanMax() {
+        Range<Integer> lowerBoundRange = Game.getLowerBoundLimits();
+
+        Game.setLowerBoundDefault(lowerBoundRange.upperEndpoint() + 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setUpperBoundShouldThrowExceptionIfArgumentLessThanMin() {
+        Range<Integer> upperBoundRange = Game.getUpperBoundLimits();
+
+        Game.setUpperBoundDefault(upperBoundRange.lowerEndpoint() - 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setUpperBoundShouldThrowExceptionIfArgumentMoreThanMax() {
+        Range<Integer> upperBoundRange = Game.getUpperBoundLimits();
+
+        Game.setUpperBoundDefault(upperBoundRange.upperEndpoint() + 1);
     }
 }
