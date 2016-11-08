89a816bd31a973e5bebce78f12607c43ac9023c6
Stoliarov Oleg <stoljarov.oleg@gmail.com>
2016-11-08 20:04:02 +0200

    Refactoring

--- src/main/java/com/stolser/javatraining/block04/recordbook/view/ViewGeneratorImpl.java
+++ src/main/java/com/stolser/javatraining/block04/recordbook/view/ViewGeneratorImpl.java
@@ -11,42 +11,67 @@ import java.util.stream.Collectors;
 
 public class ViewGeneratorImpl implements ViewGenerator {
     private static final String DATE_FORMAT = "dd:MM:YYYY HH:mm:ss";
+    private static final String RECORD_TITLE_TEXT = "----------- record %d -----------\n";
+    private static final String USER_NAME_TEXT = "User name: %s\n";
+    private static final String COMMENT_TEXT = "Comment: %s\n";
+    private static final String GROUPS_TEXT = "Groups: %s\n";
+    private static final String PHONES_TEXT = "Phones: %s\n";
+    private static final String EMAIL_TEXT = "Email: %s\n";
+    private static final String SKYPE_TEXT = "Skype: %s\n";
+    private static final String ADDRESS_TEXT = "Address: %s\n";
+    private static final String CREATION_DATE_TEXT = "Creation date: %s\n";
+    private static final String LAST_MODIFIED_DATE_TEXT = "Last modified date: %s\n\n";
+
+    private StringBuilder builder = new StringBuilder();
+    private RecordBook recordBook;
+    private Set<Record> records;
 
     @Override
     public String getRecordBookView(RecordBook recordBook) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
-        Set<Record> records = recordBook.getRecords();
-        StringBuilder builder = new StringBuilder();
-        int recordIndex = 1;
+        this.recordBook = recordBook;
+        records = recordBook.getRecords();
 
+        appendHeader();
+        appendAllRecords();
+        appendFooter();
+
+        return builder.toString();
+    }
+
+    private void appendHeader() {
         builder.append(String.format("================================== Record Book (%s) " +
                 "==================================\n", recordBook.getName()));
+    }
 
-        for (Record record: records){
+    private void appendAllRecords() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+        int recordIndex = 1;
+
+        for (Record record : records) {
             List<String> phoneStrings = record.getPhones()
                     .stream()
                     .map(UserPhone::getFullNumber)
                     .collect(Collectors.toList());
 
-            builder.append(String.format("----------- record %d -----------\n", recordIndex));
-            builder.append(String.format("User name: %s\n", record.getUserName().getFullForm()));
-            builder.append(String.format("Comment: %s\n", record.getComment()));
-            builder.append(String.format("Groups: %s\n", record.getGroups()));
-            builder.append(String.format("Phones: %s\n", phoneStrings));
-            builder.append(String.format("Email: %s\n", record.getEmail()));
-            builder.append(String.format("Skype: %s\n", record.getSkype()));
-            builder.append(String.format("Address: %s\n", record.getAddress().getFullAddress()));
-            builder.append(String.format("Creation date: %s\n",
+            builder.append(String.format(RECORD_TITLE_TEXT, recordIndex));
+            builder.append(String.format(USER_NAME_TEXT, record.getUserName().getFullForm()));
+            builder.append(String.format(COMMENT_TEXT, record.getComment()));
+            builder.append(String.format(GROUPS_TEXT, record.getGroups()));
+            builder.append(String.format(PHONES_TEXT, phoneStrings));
+            builder.append(String.format(EMAIL_TEXT, record.getEmail()));
+            builder.append(String.format(SKYPE_TEXT, record.getSkype()));
+            builder.append(String.format(ADDRESS_TEXT, record.getAddress().getFullAddress()));
+            builder.append(String.format(CREATION_DATE_TEXT,
                     dateFormat.format(record.getCreationDate().toEpochMilli())));
-            builder.append(String.format("Last modified date: %s\n\n",
+            builder.append(String.format(LAST_MODIFIED_DATE_TEXT,
                     dateFormat.format(record.getLastModifDate().toEpochMilli())));
 
             recordIndex++;
         }
+    }
 
+    private void appendFooter() {
         builder.append("======================================= Footer " +
                 "========================================\n");
-
-        return builder.toString();
     }
 }
