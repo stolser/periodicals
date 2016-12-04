abf0ef93f2ce06e61adf17118388b66642a564b3
Stoliarov Oleg <stoljarov.oleg@gmail.com>
2016-12-04 23:57:52 +0200

    Add a charset filter

---
+++ src/main/java/com/stolser/javatraining/webproject/controller/CharsetFilter.java
@@ -0,0 +1,24 @@
+package com.stolser.javatraining.webproject.controller;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class CharsetFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        request.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
